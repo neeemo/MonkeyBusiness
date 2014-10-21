b2286fc13ea578774167393c27cdc8c149acf61b
Oscar Söderlund <poscar@spotify.com>
2014-10-21 19:01:10 +0200

    Homogenize with other starter kits

---
+++ src/main/java/com/monkeymusicchallenge/warmup/AI.java
@@ -0,0 +1,116 @@
+package com.monkeymusicchallenge.warmup;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class AI {
+
+  public String move(final JSONObject gameState) {
+
+    // Every game has a limited number of turns. Use every turn wisely!
+    final int remainingNumberOfTurns = gameState.getInt("turns");
+
+    // The level layout is a 2D-matrix (an array of arrays).
+    //
+    // Every element in the matrix is a string. The string tells you what's
+    // located at the corresponding position on the level.
+    //
+    // In the warmup challenge, your objective is to find all music items
+    // and deliver them to the eagerly waiting Spotify user.
+    //
+    // "empty": an empty tile, you can move here
+    // "monkey": your monkey, this is where you're currently at
+    // "song" / "album" / "playlist": a music item, go get them!
+    // "user": go here once you've picked up all music items
+    //
+    // Too easy for you? Good...
+    //
+    // The real fun begins when the warmup is over and the competition begins!
+    final JSONArray currentLevelLayout = gameState.getJSONArray("layout");
+
+    // This is an array of all music items you've currently picked up
+    final JSONArray pickedUpMusicItems = gameState.getJSONArray("pickedUp");
+
+    // The position attribute tells you where your monkey is
+    final JSONArray currentPositionOfMonkey = gameState.getJSONArray("position");
+
+    // Speaking of positions...
+    //
+    // X and Y coordinates can be confusing.
+    // Which way is up and which way is down?
+    //
+    // Here is an example explaining how coordinates work in
+    // Monkey Music Challenge:
+    //
+    // {
+    //   "layout": [["empty", "monkey"]
+    //              ["song",  "empty"]]
+    //   "position": [0, 1],
+    //   ...
+    // }
+    //
+    // The "position" attribute tells you the location of your monkey
+    // in the "layout" matrix. In this example, you're at layout[0][1].
+    //
+    // If you send { "command": "move", "direction": "down", ... }
+    // to the server, you'll get back:
+    //
+    // {
+    //   "layout": [["empty", "empty"]
+    //              ["song",  "monkey"]]
+    //   "position": [1, 1]
+    // }
+    //
+    // If you instead send { "command": "move", "direction": "left", ... }
+    // to the server, you'll get back:
+    //
+    // {
+    //   "layout": [["monkey", "empty"]
+    //              ["song",   "empty"]]
+    //   "position": [0, 0]
+    // }
+    //
+    // So what about picking stuff up then?
+    //
+    // It's simple!
+    //
+    // Just stand next to something you want to pick up and move towards it.
+    //
+    // For example, say our current game state looks like this:
+    //
+    // {
+    //   "layout": [["empty", "empty"]
+    //              ["song",  "monkey"]]
+    //   "position": [1, 1],
+    //   "pickedUp": []
+    // }
+    //
+    // When you send { "command": "move", "direction": "left", ... }
+    // to the server, you'll get back:
+    //
+    //   "layout": [["empty",  "empty"]
+    //              ["empty",  "monkey"]]
+    //   "position": [1, 1],
+    //   "pickedUp": ["song"],
+    //   ...
+    // }
+    //
+    // Instead of moving, your monkey successfully picked up the song!
+    //
+    // Got it? Sweet! This message will self destruct in five seconds...
+
+    // This will always return the string "monkey" - get it?
+    final String monkey = currentLevelLayout
+        .getJSONArray(currentPositionOfMonkey.getInt(0))
+        .getString(currentPositionOfMonkey.getInt(1));
+
+    // TODO: You may want to do something smarter here
+    return this.randomDirection();
+  }
+
+  private String randomDirection() {
+    return new String[] {"up", "down", "left", "right"}[ThreadLocalRandom.current().nextInt(4)];
+  }
+}
---
+++ src/main/java/com/monkeymusicchallenge/warmup/Main.java
@@ -0,0 +1,108 @@
+package com.monkeymusicchallenge.warmup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+// Hi! Welcome to the Monkey Music Challenge Java starter kit!
+
+public class Main {
+
+  // You control your monkey by sending POST requests to the Monkey Music server
+  private static final String SERVER_URL = "http://warmup.monkeymusicchallenge.com";
+
+  public static void main(final String[] args) {
+
+    // Don't forget to provide the right command line arguments
+    if (args.length < 2) {
+      System.out.println("Usage: java -jar target/warmup.jar <your-team-name> <your-api-key\n");
+      if (args.length < 1) {
+        System.out.println(" Missing argument: <your-team-name>");
+      }
+      if (args.length < 2) {
+        System.out.println(" Missing argument: <your-api-key>");
+      }
+      System.exit(1);
+    }
+
+    // You identify yourselves by your team name and your API key
+    final String teamName = args[0];
+    final String apiKey = args[1];
+
+    // You POST to a team-specific URL:
+    //   warmup.monkeymusicchallenge.com/team/<your-team-name>
+    //
+    // Surf to this URL and watch your monkey carry out your commands!
+    final String teamUrl = SERVER_URL + "/team/" + teamName;
+
+    // We've put the AI-code in a separate class
+    final AI ai = new AI();
+
+    // Allright, time to get started!
+
+    // When we POST a command to the server, it always replies with the current game state
+    JSONObject currentGameState = postToServer(teamUrl,
+        "command", "new game",
+        "apiKey", apiKey);
+
+    // The current game state tells you if you have any turns left to move
+    while (currentGameState.getInt("turns") > 0) {
+      System.out.println("Remaining turns: " + currentGameState.getInt("turns"));
+
+      // Use your AI to decide in which direction to move
+      final String nextMoveDirection = ai.move(currentGameState);
+
+      // ...and send a new move command to the server
+      currentGameState = postToServer(teamUrl,
+          "command", "move",
+          "direction", nextMoveDirection,
+          "apiKey", apiKey);
+
+      // After sending your next move, you'll get the new game state back
+    }
+
+    // If the game is over, our server will tell you how you did
+    System.out.println("\nGame over!\n");
+    // Go to http://warmup.monkeymusicchallenge.com/team/<your-team-name> for more details
+    System.out.println("  " + currentGameState.getString("message"));
+    System.exit(0);
+  }
+
+  // Every time we POST a command to the server, we get the current game state back
+  private static JSONObject postToServer(final String teamUrl, final String... args) {
+    final Map<String, Object> messageToPost = new HashMap<String, Object>();
+    for (int i = 0; i < args.length; i += 2) {
+      final String key = args[i];
+      final String value = args[i + 1];
+      messageToPost.put(key, value);
+    }
+
+    try {
+      // In this starter kit, we use the Unirest library to send POST requests
+      final HttpResponse<JsonNode> response = Unirest.post(teamUrl)
+          .header("Content-Type", "application/json")
+          .body(new JSONObject(messageToPost).toString())
+          .asJson();
+
+      if (response.getCode() == 200) {
+        return response.getBody().getObject();
+      } else {
+        throw new RuntimeException(
+            "Server replied with status code " + response.getCode()
+            + ", " + response.getBody().getObject().getString("message"));
+      }
+    } catch (final Exception e) {
+      // Hopefully, our server will always be able to handle your requests, but you never know...
+      e.printStackTrace();
+      System.out.println("Error! We seem to have  trouble talking to the server...\n");
+      System.out.println("  " + e.getMessage());
+      System.exit(1);
+      throw new AssertionError(); // unreachable
+    }
+  }
+}
